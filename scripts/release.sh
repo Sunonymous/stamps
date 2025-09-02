00258698bae34b4f678abf9f37f13659a59107c4
Sunny <embodiedsunshine@proton.me>
2025-09-02 17:37:46 -0600

    tweak build scripts and base files

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,9 +5,8 @@ npx shadow-cljs release app
 
 echo "[2/3] Compilation complete. Copying necessary files..."
 mkdir -p release
-cp resources/public/index.html      release/
-cp resources/public/app.css         release/
-cp resources/public/styles.css      release/
-cp resources/public/js/main.js      release/js/
+cp ./resources/public/index.html          release/
+cp ./resources/public/styles.css          release/
+cp ./resources/public/js/compiled/app.js  release/
 
 echo "[3/3] Release complete. Files in release/ directory."
